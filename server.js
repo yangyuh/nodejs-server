0ffb633d63ef40ce61bf12026d08659862a4409f
yangyh <136585913@qq.com>
2019-06-27 17:29:50 +0800

    first commit

---
+++ server.js
@@ -0,0 +1,11 @@
+// vue HTML5 History 模式express 中间件
+var history = require('connect-history-api-fallback');
+var express = require('express');
+var path = require('path');
+var app = express();
+app.use(history());
+//在express中使用的时候需要注意一点的是，该中间件必须要放在express.static中间前的前面引入
+app.use(express.static(path.join(__dirname, 'dist')));
+app.listen(3001,function(){
+	console.log("server start in 3001" )
+})
